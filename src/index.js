bb3217101d16a00a014dacbc35dab192506d93d3
jcdeccechis <jcdeccechis@technisys.com>
2022-06-26 11:06:42 -0300

    sagas added

--- src/index.js
+++ src/index.js
@@ -2,16 +2,30 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+import { createStore, applyMiddleware, compose } from "redux";
+import { Provider } from "react-redux";
+import reducer from "./reducer";
+
+import createSagaMiddleware from 'redux-saga'
+import rootSaga from './sagas'
+
+const sagaMiddleware = createSagaMiddleware()
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+let store = createStore(
+  reducer,
+  composeEnhancers(applyMiddleware(
+    sagaMiddleware
+  ))
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+sagaMiddleware.run(rootSaga)
+
+ReactDOM.render(
+<Provider store={store}>
+  <App />
+</Provider>, document.getElementById('root'));
+registerServiceWorker();
---
+++ src/sagas.js
@@ -0,0 +1,8 @@
+import { delay } from 'redux-saga'
+import { call, put, takeEvery, all, select, takeLatest } from 'redux-saga/effects'
+import { types } from './actions'
+
+export default function* rootSaga() {
+  yield all([
+  ])
+}
